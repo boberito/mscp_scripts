ae278c872c67ba72d63baa523f243cea223b10a0
Bob Gendler <boberito@mac.com>
2026-08-22 22:38:24 -0400

    Updated generate_tenable for mscp 2.0, using mscp libraries

--- generate_tenable.py
+++ generate_tenable.py
@@ -1,136 +1,40 @@
-#!/usr/bin/env python3
-
+#! /usr/bin/env python
 #use just like generate_guidance.py - point to a baseline file. Boom.
+from mscp import RuleLibrary
+from mscp import Macsecurityrule
+from mscp import Baseline
 import argparse
 import sys
 import os
 import os.path
-import yaml
-import glob
 import warnings
 from pathlib import Path
 
-def get_rule_yaml(rule_file, custom=False):
-    """ Takes a rule file, checks for a custom version, and returns the yaml for the rule
+from mscp.classes import baseline, references
+
+def validate_file(arg: str) -> Path | None:
+    """`argparse` type validator: ensure `arg` points at an existing file.
+
+    Used as the `type=` argument on flags that take a path. Logs an
+    error and calls `sys.exit` if the path doesn't resolve to a file.
+
+    Args:
+        arg (str): Raw command-line argument value.
+
+    Returns:
+        Path | None: The validated `Path`, or never returns when the file
+            is missing (process exits).
     """
-    resulting_yaml = {}
-    names = [os.path.basename(x) for x in glob.glob('../custom/rules/**/*.yaml', recursive=True)]
-    file_name = os.path.basename(rule_file)
-    
-    if custom:
-        print(f"Custom settings found for rule: {rule_file}")
-        try:
-            override_path = glob.glob('../custom/rules/**/{}'.format(file_name), recursive=True)[0]
-        except IndexError:
-            override_path = glob.glob('../custom/rules/{}'.format(file_name), recursive=True)[0]
-        with open(override_path) as r:
-            rule_yaml = yaml.load(r, Loader=yaml.SafeLoader)
+    if (file := Path(arg)).is_file():
+        return file
     else:
-        with open(rule_file) as r:
-            rule_yaml = yaml.load(r, Loader=yaml.SafeLoader)
-    
-    try:
-        og_rule_path = glob.glob('../rules/**/{}'.format(file_name), recursive=True)[0]
-    except IndexError:
-        #assume this is a completely new rule
-        og_rule_path = glob.glob('../custom/rules/**/{}'.format(file_name), recursive=True)[0]
-        resulting_yaml['customized'] = ["customized rule"]
-    
-    # get original/default rule yaml for comparison
-    with open(og_rule_path) as og:
-        og_rule_yaml = yaml.load(og, Loader=yaml.SafeLoader)
-
-    for yaml_field in og_rule_yaml:
-        #print('processing field {} for rule {}'.format(yaml_field, file_name))
-        if yaml_field == "references":
-            if not 'references' in resulting_yaml:
-                resulting_yaml['references'] = {}
-            for ref in og_rule_yaml['references']:
-                try:
-                    if og_rule_yaml['references'][ref] == rule_yaml['references'][ref]:
-                        resulting_yaml['references'][ref] = og_rule_yaml['references'][ref]
-                    else:
-                        resulting_yaml['references'][ref] = rule_yaml['references'][ref]
-                except KeyError:
-                    #  reference not found in original rule yaml, trying to use reference from custom rule
-                    try:
-                        resulting_yaml['references'][ref] = rule_yaml['references'][ref]
-                    except KeyError:
-                        resulting_yaml['references'][ref] = og_rule_yaml['references'][ref]
-                try: 
-                    if "custom" in rule_yaml['references']:
-                        resulting_yaml['references']['custom'] = rule_yaml['references']['custom']
-                        if 'customized' in resulting_yaml:
-                            if 'customized references' not in resulting_yaml['customized']:
-                                resulting_yaml['customized'].append("customized references")
-                        else:
-                            resulting_yaml['customized'] = ["customized references"]
-                except:
-                    pass
-            
-        else: 
-            try:
-                if og_rule_yaml[yaml_field] == rule_yaml[yaml_field]:
-                    #print("using default data in yaml field {}".format(yaml_field))
-                    resulting_yaml[yaml_field] = og_rule_yaml[yaml_field]
-                else:
-                    #print('using CUSTOM value for yaml field {} in rule {}'.format(yaml_field, file_name))
-                    resulting_yaml[yaml_field] = rule_yaml[yaml_field]
-                    if 'customized' in resulting_yaml:
-                        resulting_yaml['customized'].append("customized {}".format(yaml_field))
-                    else:
-                        resulting_yaml['customized'] = ["customized {}".format(yaml_field)]
-            except KeyError:
-                resulting_yaml[yaml_field] = og_rule_yaml[yaml_field]
-
-    return resulting_yaml
-
-def fill_in_odv(resulting_yaml, parent_values):
-    fields_to_process = ['title', 'discussion', 'check', 'fix']
-    _has_odv = False
-    if "odv" in resulting_yaml:
-        try:
-            if type(resulting_yaml['odv'][parent_values]) == int:
-                odv = resulting_yaml['odv'][parent_values]
-            else:
-                odv = str(resulting_yaml['odv'][parent_values])
-            _has_odv = True
-        except KeyError:
-            try:
-                if type(resulting_yaml['odv']['custom']) == int:
-                    odv = resulting_yaml['odv']['custom']
-                else:
-                    odv = str(resulting_yaml['odv']['custom'])
-                _has_odv = True
-            except KeyError:
-                if type(resulting_yaml['odv']['recommended']) == int:
-                    odv = resulting_yaml['odv']['recommended']
-                else:
-                    odv = str(resulting_yaml['odv']['recommended'])
-                _has_odv = True
-        else:
-            pass
-
-    if _has_odv:
-        for field in fields_to_process:
-            if "$ODV" in resulting_yaml[field]:
-                resulting_yaml[field]=resulting_yaml[field].replace("$ODV", str(odv))
-
-        for result_value in resulting_yaml['result']:
-            if "$ODV" in str(resulting_yaml['result'][result_value]):
-                resulting_yaml['result'][result_value] = odv
-
-        if resulting_yaml['mobileconfig_info']:
-            for mobileconfig_type in resulting_yaml['mobileconfig_info']:
-                if isinstance(resulting_yaml['mobileconfig_info'][mobileconfig_type], dict):
-                    for mobileconfig_value in resulting_yaml['mobileconfig_info'][mobileconfig_type]:
-                        if "$ODV" in str(resulting_yaml['mobileconfig_info'][mobileconfig_type][mobileconfig_value]):
-                            resulting_yaml['mobileconfig_info'][mobileconfig_type][mobileconfig_value] = odv
+        print(f"File Not found: {arg}")
+        sys.exit()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Given a profile, create JSON custom schema to use in Jamf.')
-    parser.add_argument("baseline", default=None, help="Baseline YAML file used to create the JSON custom schema.", type=argparse.FileType('rt'))
+    parser = argparse.ArgumentParser(description='Generate Tenable custom audit file from a baseline YAML file.')
+    parser.add_argument("baseline", default=None, help="Baseline YAML file used to create the Tenable custom audit file.", type=validate_file)
 
     results = parser.parse_args()
     try:
@@ -160,125 +64,126 @@ def main():
         
     except IOError as msg:
         parser.error(str(msg))
+    
+    baseline = Baseline.from_yaml(results.baseline,"en")    
+
+    
+#     version_file = "../VERSION.yaml"
+#     with open(version_file) as r:
+#         version_yaml = yaml.load(r, Loader=yaml.SafeLoader)
 
 
-    version_file = "../VERSION.yaml"
-    with open(version_file) as r:
-        version_yaml = yaml.load(r, Loader=yaml.SafeLoader)
-
-
-    profile_yaml = yaml.load(results.baseline, Loader=yaml.SafeLoader)
-    tenable = '''
-    <check_type:"Unix">
+#     profile_yaml = yaml.load(results.baseline, Loader=yaml.SafeLoader)
+    tenable = '''<check_type:"Unix">
 <if>
   <condition type:"AND">
     <custom_item>
       type        : CMD_EXEC
-      description : "MacOS {} is installed"
-      cmd         : "/usr/bin/sw_vers | /usr/bin/grep 'ProductVersion'"
-      expect      : "^ProductVersion[\\\s]*:[\\\s]*{}"
+      description : "{} {} is installed"
+      cmd         : "/usr/bin/sw_vers | /usr/bin/grep 'ProductVersion'"      
+      expect      : "{}{}"
     </custom_item>
 </condition>
-    '''.format(version_yaml['os'],version_yaml['os'].split('"')[0].split('.')[0])
-    githubBranch = version_yaml['version'].split(" ")[0].lower()
+    '''.format(baseline.platform['os'],baseline.platform['version'],r'^ProductVersion[\\s]*:[\\s]',baseline.platform['version'])    
     tenable = tenable + '''
 <then>
     <report type:"PASSED">
       description : "{}"
-      see_also    : "https://github.com/usnistgov/macos_security/blob/{}"
+      see_also    : "https://github.com/usnistgov/macos_security"
+      show_output : YES
 </report>
     
-    '''.format(profile_yaml['title'],githubBranch)
-    # https://github.com/usnistgov/macos_security/blob/ventura/rules/os/os_airdrop_disable.yaml
-    for sections in profile_yaml['profile']:
-        if sections['section'] == "Supplemental":
+    '''.format(baseline.title)
+#     # https://github.com/usnistgov/macos_security/blob/ventura/rules/os/os_airdrop_disable.yaml
+
+    for section in baseline.profile:
+        if section.section == "Supplemental" or section.section == "Excluded":
             continue
-        
-        for profile_rule in sections['rules']:
-            
-
-            if glob.glob('../custom/rules/**/{}.yaml'.format(profile_rule),recursive=True):
-                rule = glob.glob('../custom/rules/**/{}.yaml'.format(profile_rule),recursive=True)[0]
-                custom=True
-            
-            elif glob.glob('../rules/*/{}.yaml'.format(profile_rule)):
-                rule = glob.glob('../rules/*/{}.yaml'.format(profile_rule))[0]
-                custom=False
-            
-            rule_yaml = get_rule_yaml(rule, custom)
-
-            
-            if "inherent" in rule_yaml['tags'] or "n_a" in rule_yaml['tags'] or "permanent" in rule_yaml['tags']:
-                continue
-
-            try:
-                parent_values = profile_yaml['parent_values']
-            except KeyError:
-                parent_values = "recommended"
-
-            fill_in_odv(rule_yaml,parent_values)
-            mscp_result = ""
-            try:
-
-                for keys,value in rule_yaml['result'].items():
-                    mscp_result = value
-            
-            except:
-                mscp_result = ""
-
+        for rule in section.rules:
+                     
             references = ""
-            for values in rule_yaml['references']:
-                for v in rule_yaml['references'][values]:
-                    if v == "N/A":
+            for references, v in rule.references:
+                if v == None:
+                    continue                
+                for ref_name, ref_value in v:
+                    if ref_value == None:
                         continue
-                    elif v == "benchmark":
-                        continue
-                    elif v == "controls v8":
-                        for v8 in rule_yaml['references'][values]["controls v8"]:
-                            references = references + "{}|{},".format("cis_v8",v8)
-                    else:
-                        references = references + "{}|{},".format(values,v)
+                    if ref_name == "cce":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("CCE",values)
+                    if ref_name == "nist_800_171r3":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("800-171r3",values)
+                    if ref_name == "nist_800_53r5":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("800-53r5",values)
+                    if ref_name == "disa_stig":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("STIG-ID",values)
+                    if ref_name == "cmmc":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("CMMC",values)
+                    if ref_name == "cci":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("CCI",values)
+                    if ref_name == "srg":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("SRG",values)
+                    if ref_name == "bio":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("BIO",values)
+                    if ref_name == "hicp":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("HICP",values)
+                    if ref_name == "benchmark":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("CIS_Benchmark",values)
+                    if ref_name == "controls_v8":
+                        for values in ref_value:
+                            references = references + "{}|{},".format("CIS_V8",values)
+                    references = references + ","
             references.rstrip()
-            if references == "":
-                references = "" 
-            elif references[-1] == ",":
-                references = references.rstrip(references[-1])
 
-            if "inherent" in rule_yaml['tags']:
+            #   reference   : "800-171|3.4.1,800-171|3.4.2,800-171|3.4.6,800-171|3.4.7,800-171|3.13.1,800-171|3.13.2,800-171r3|03.04.01,800-171r3|03.04.02,800-171r3|03.04.06,800-171r3|03.16.01,800-53|CM-2,800-53|CM-6,800-53|CM-7,800-53|CM-7(1),800-53|CM-9,800-53|SA-3,80OTH-53|SA-8,800-53|SA-10,8００－５３ｒ５｜ＣＭ－１，８００－５３ｒ５｜ＣＭ－２，８００－５３ｒ５｜ＣＭ－６，８００－５３ｒ５｜ＣＭ－７，８００－５３ｒ５｜ＣＭ－７（１），８００－５３ｒ５｜ＣＭ－９，８００－５３ｒ５｜ＳＡ－３，８００－５３ｒ５｜ＳＡ－８，８００－５３ｒ５｜ＳＡ－１０，ＣＳＣｖ７｜１６．１１，ＣＳＣｖ８｜４．１，ＣＳＦ｜ＤＥ．ＡＥ－１，ＣＳＦ｜ＰＲ．ＤＳ－７，ＣＳＦ｜ＰＲ．ＩＰ－１，ＣＳＦ｜ＰＲ．ＩＰ－２，ＣＳＦ｜ＰＲ．ＩＰ－３，ＣＳＦ｜ＰＲ．ＰＴ－３，ＣＳＦ２．０｜ＤＥ．ＣＭ－０９，ＣＳＦ２．０｜ＩＤ．ＡＭ－０８，ＣＳＦ２．０｜ＩＤ．ＩＭ－０１，ＣＳＦ２．０｜ＩＤ．ＩＭ－０２，ＣＳＦ２．０｜ＩＤ．ＩＭ－０３，ＣＳＦ２．０｜ＩＤ．ＲＡ－Ｏ９，ＣＳＦ２．Ｏ｜ＰＲ．ＤＳ－１Ｏ，ＣＳＦ２．Ｏ｜ＰＲ．ＩＲ－Ｏ３，ＣＳＦ２．Ｏ｜ＰＲ．ＰＳ－Ｏ１，ＣＳＦ２．Ｏ｜ＰＲ．ＰＳ－Ｏ６，ＧＤＰＲ｜３２．１ｂ，ＨＩＰＡＡ｜１６４．３０６（ａ）（１），ＩＳＯ＿２７００１＿２₀₂₂｜Ａ．５．２，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．５．８，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．９，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．２５，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．２６，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．２７，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．２８，ＩＳＯ＿２７₀₀₁＿₂₀₂₂｜Ａ．８．３０，Ｉ🇸🇴＿𝟐𝟕𝟎𝟎𝟏＿𝟐𝟎𝟐𝟐｜𝑨。𝟖。𝟑𝟏، 𝑰𝑺𝑶＿𝟐𝟕𝟎𝟎𝟏＿𝟐𝟎𝟐𝟐}|𝑨。 𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。  𝒃。
+            #                                                       
+              
+           
+
+            if "inherent" in rule.tags:
                 tenable = tenable + '''
 <report type:"PASSED">
     description : "{0}"
     info        : "{1}"
     reference   : "{2}"
-    see_also    : "https://github.com/usnistgov/macos_security/blob/{3}/rules/{4}/{5}.yaml"
-</report>'''.format(rule_yaml['title'],rule_yaml['discussion'].replace('"','\\"').rstrip(),references,githubBranch,rule_yaml['id'].split("_")[0],rule_yaml['id'])
+    see_also    : "https://github.com/usnistgov/macos_security/blob/main/src/mscp/data/rules/{3}/{4}.yaml"
+</report>'''.format(rule.title,rule.discussion.replace('"','\\"').rstrip(),references,rule.rule_id.split("_")[0],rule.rule_id)
     # https://github.com/usnistgov/macos_security/blob/ventura/rules/os/os_airdrop_disable.yaml
 
-            elif "permanent" in rule_yaml['tags']:
+            elif "permanent" in rule['tags']:
                 tenable = tenable + '''
 <report type:"WARNING">
     description : "{0}"
     info        : "{1}"
     reference   : "{2}"
-    see_also    : "https://github.com/usnistgov/macos_security/blob/{3}/rules/{4}/{5}.yaml"
-</report>'''.format(rule_yaml['title'],rule_yaml['discussion'].replace('"','\\"').rstrip(),references,githubBranch,rule_yaml['id'].split("_")[0],rule_yaml['id'])
+    see_also    : "https://github.com/usnistgov/macos_security/blob/main/src/mscp/data/rules/{3}/{4}.yaml"
+</report>'''.format(rule.title,rule.discussion.replace('"','\\"').rstrip(),references,rule.rule_id.split("_")[0],rule.rule_id)
                 
-            elif "n_a" in rule_yaml['tags']:
+            elif "n_a" in rule['tags']:
                 tenable = tenable
 
-            elif "manual" in rule_yaml['tags']:
+            elif "manual" in rule['tags']:
                 tenable = tenable + '''
 <report type:"WARNING">
     description : "{0}"
     info        : "{1}"
     reference   : "{2}"
-    see_also    : "https://github.com/usnistgov/macos_security/blob/{3}/rules/{4}/{5}.yaml"
-</report>'''.format(rule_yaml['title'],rule_yaml['discussion'].replace('"','\\"').rstrip(),references,githubBranch,rule_yaml['id'].split("_")[0],rule_yaml['id'])
+    see_also    : "https://github.com/usnistgov/macos_security/blob/main/src/mscp/data/rules/{3}/{4}.yaml"
+    </report>'''.format(rule.title,rule.discussion.replace('"','\\"').rstrip(),references,rule.rule_id.split("_")[0],rule.rule_id)
             
             else:
-                rule_yaml['check'] = rule_yaml['check'].replace('\\','\\\\')
-                if "CURRENT_USER" in rule_yaml['check']:
-                    rule_yaml['check'] = rule_yaml['check'].replace("$CURRENT_USER","$( /usr/sbin/scutil <<< \"show State:/Users/ConsoleUser\" | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }' )")
+                rule.check = rule.check.replace('\\','\\\\')
+                if "CURRENT_USER" in rule.check:
+                    rule.check = rule.check.replace("$CURRENT_USER","$( /usr/sbin/scutil <<< \"show State:/Users/ConsoleUser\" | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }' )")
 
                 tenable = tenable + '''
 <custom_item>
@@ -287,10 +192,10 @@ def main():
     description : "{0}"
     info        : "{1}"
     reference   : "{4}"
-    see_also    : "https://github.com/usnistgov/macos_security/blob/{5}/rules/{6}/{7}.yaml"
+    see_also    : "https://github.com/usnistgov/macos_security/blob/main/src/mscp/data/rules/{5}/{6}.yaml"
     cmd         : "{2}"
     expect      : "{3}"
-</custom_item>'''.format(rule_yaml['title'],rule_yaml['discussion'].replace('"','\\"').rstrip(),rule_yaml['check'].replace('"','\\"').rstrip(),mscp_result,references,githubBranch,rule_yaml['id'].split("_")[0],rule_yaml['id'])
+</custom_item>'''.format(rule.title,rule.discussion.replace('"','\\"').rstrip(),rule.check.replace('"','\\"').rstrip(),rule.result_value,references,rule.rule_id.split("_")[0],rule.rule_id)
     
     
 
@@ -301,13 +206,13 @@ def main():
     <report type:"WARNING">
       description : "{}"
       info        : "NOTE: Nessus has not identified that the chosen audit applies to the target device."
-      see_also    : "https://github.com/usnistgov/macos_security/blob/{}/rules/{}/{}.yaml"
+      see_also    : "https://pages.nist.gov/macos_security"
     </report>
   </else>
 </if>
 
 </check_type>
-'''.format(profile_yaml['title'],githubBranch,rule_yaml['id'].split("_")[0],rule_yaml['id'])
+'''.format(baseline.title)
     with open(output,'w') as rite:
             rite.write(tenable)
             rite.close()
