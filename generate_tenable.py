2f2840418bf4e653e4381e00a0119d258a9372fa
Bob Gendler <boberito@mac.com>
2026-08-24 13:48:28 -0400

    Update generate_tenable.py with custom rules instructions
    Added instructions for using custom rules in generate_tenable.py.

--- generate_tenable.py
+++ generate_tenable.py
@@ -6,6 +6,9 @@
 # pip install git+https://github.com/usnistgov/macos_security@main
 # ./generate_tenable.py <baseline.yaml>
 
+# in order to use custom rules - in the same directory as the script. Create a folder custom, and within that rules. You can drop your ODV or custom rules in there. 
+# they can be in folders with the different sections as they are in the main rules library or just flat within the custom folder.
+
 from mscp import RuleLibrary
 from mscp import Macsecurityrule
 from mscp import Baseline
